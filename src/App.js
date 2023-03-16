28258bb38bfc295ec5dea5bd9aa8c01724bb3137
Jovan <jovan.reyes.dev@gmail.com>
2023-03-17 02:01:09 +0800

    update metadata in index.html

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
 import Homepage from 'components/Homepage';
 import Footer from 'components/Footer';
-import MetaThumbnail from 'assets/images/jovanportraitbrush.png';
 
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <Helmet>
-        <meta property="og:image" content={MetaThumbnail} />
-      </Helmet>
       <Homepage />
       <Footer />
     </div>
